40350c6487ab1c5bc01b426729465edbd848408f
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-12-13 12:38:38 +0100

    Add custom exception classes for authentication and authorization errors

--- src/ercaspy/error.py
+++ src/ercaspy/error.py
@@ -14,6 +14,36 @@ class ErcasBadRequestError(ErcasError):
     pass
 
 
+class ErcasUnauthorizedError(ErcasError):
+    """Exception raised for errors in the authentication.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    pass
+
+
+class ErcasForbiddenError(ErcasError):
+    """Exception raised for errors in the authorization.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    pass
+
+
+class ErcasNotFoundError(ErcasError):
+    """Exception raised for errors in the resource not found.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    pass
+
+
 class ErcasUnprocessableError(ErcasError):
     """Exception raised for errors that are not processable.
 
